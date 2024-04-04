2fc264ffbb4fe521588ba3f318780aa082a127ab
EDUARDO ANDRADE <edu.matos0107@gmail.com>
2024-04-04 19:09:57 -0300

    COLOCANDO HTML NO PROJETO INICIAL

--- ReceitaApp/views.py
+++ ReceitaApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+def listar_receitas(request):
+    nome = 'Eduardo'
+    ingredientes = ['Farinha', 'Ovo', 'Manteiga', 'Leite']
+    # dicionario que vai levar os dados para o template
+    # chave : valor
+    context = {
+        'Endereco' : 'Av. Marechal tito',
+        'Bairro' : 'São Miguel Paulista', 
+        'Cidade' : 'São Paulo',
+        'Estado' : 'SP',
+        'Nome' : nome,
+        'Ingredientes' : ingredientes
+
+    }
+    # qual template essa view ira retornar
+    return render(request, 'receitas.html', context)
